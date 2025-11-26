3d91ef218e6ee4907b85c2c5c31cb385b0744fb5
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-11-26 14:10:21 +0500

    Compile YOLOv8 model to TensorRT engine

--- src/main.py
+++ src/main.py
@@ -266,3 +266,5 @@ if __name__ == '__main__':
 # Add FP16 fallback on unsupported operations [2025-11-20T20:24:30]
 
 # Add dynamic batching Support in engine [2025-11-24T14:07:58]
+
+# Compile YOLOv8 model to TensorRT engine [2025-11-26T14:10:21]
