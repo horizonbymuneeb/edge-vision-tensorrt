1301b136f5762ccef7d50db6165b444343e49da7
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-02-06 10:41:23 +0500

    Update TensorRT plugin structure for new API

--- src/utils.py
+++ src/utils.py
@@ -179,3 +179,5 @@ def format_number(n: int) -> str:
 # Implement engine serialization for Jetson [2026-02-01T09:39:29]
 
 # Profile GPU memory during batch size sweep [2026-02-02T17:20:08]
+
+# Update TensorRT plugin structure for new API [2026-02-06T10:41:23]
