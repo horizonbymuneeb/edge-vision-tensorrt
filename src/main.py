565de47550ebb53d00d26a4fe5fa8656dc13eb00
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-02-11 19:44:15 +0500

    Optimize letterbox resize kernel Performance

--- src/main.py
+++ src/main.py
@@ -318,3 +318,5 @@ if __name__ == '__main__':
 # Optimize kernel launch overhead reduction [2026-02-04T11:21:42]
 
 # Fix bounding box NMS threshold calculation [2026-02-05T19:06:02]
+
+# Optimize letterbox resize kernel Performance [2026-02-11T19:44:15]
